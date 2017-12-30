00aab308db9340e3832f3c7badfeb370b6762473
Дмитрий <ker.laeda737@yandex.ru>
2017-12-30 13:09:31 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ PatternSingleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Initialize();
+
+            Console.WriteLine(s1.GetHashCode());
+
+            Singleton s2 = Singleton.Initialize();
+
+            Console.WriteLine(s2.GetHashCode());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PatternSingleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    class Singleton
+    {
+        private static Singleton single = null;
+
+
+        protected Singleton() { }
+
+        public static Singleton Initialize()
+        {
+            if (single == null)
+                single = new Singleton();
+            return single;
+        }
+    }
+}
